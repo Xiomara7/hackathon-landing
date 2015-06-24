6b1188344793e7c901006fe7d4dcc78a7cef4e6d
Xiomara7 <figueroaxiomara7@gmail.com>
2015-06-24 16:08:09 -0400

    uploading to keroku

--- hello.py
+++ hello.py
@@ -1,31 +1,31 @@
 import os
 from flask import Flask
-from flask import render_template, request, json
+from flask import render_template, request, json, flash
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:////tmp/test.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
 class Entry(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   Fullname = db.Column(db.String(60))
-  Email = db.Column(db.String(60), unique=True)
+  Email = db.Column(db.String(60))
 
   def __init__(self, Fullname, Email):
     self.Fullname = Fullname
     self.Email = Email
 
   def __repr__(self):
-    return "Informacion de - %s" % self.Fullname
+    return "Nombre - %s e Email - %s" % self.Fullname, self.Email
 
 @app.route('/', methods=["GET", "POST"])
 def form():
   if request.method == 'GET':
     return render_template('home-fullwidth.html')
   elif request.method == 'POST':
-    print 'Es un post'
     args = {}
+
     # Get form parameters
     getEmail = request.form.get('Email', '')
 
@@ -37,11 +37,16 @@ def form():
       args["Fullname"] = request.form.get("Fullname", "n/a")
       args["Email"] = request.form.get("Email", "n/a")
     
-      entry = Entry(**args)
+      entry = Entry(args["Fullname"], args["Email"])
       db.session.add(entry)
       db.session.commit()
 
-    return render_template("thanks.html")
+      flash('Thanks! We will be in touch with you soon!')
+
+    #return render_template('home-fullwidth.html')
+    else:
+      flash('You are already in our list! We will contact you soon!')
+    #return render_template('home-fullwidth.html')
 
 @app.route('/more-info', methods=["GET", "POST"])
 def sponsors():
